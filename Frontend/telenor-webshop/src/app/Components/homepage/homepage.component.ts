1e339d9cfdf07998a3d8501093e2b653b6b86d70
Ruszin Peter <ruszin.peter@gmail.com>
2019-09-05 20:22:02 +0200

    WIP pipe

--- Frontend/telenor-webshop/src/app/Components/homepage/homepage.component.ts
+++ Frontend/telenor-webshop/src/app/Components/homepage/homepage.component.ts
@@ -6,27 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent implements OnInit {
-  items = [{
-    id: "43212",
-    name: " galaxy S",
-    brand: "Samsung"
-  },
-  {
-    id: "432123",
-    name: " galaxy G",
-    brand: "Samsung"
-  },
-  {
-    id: "43312",
-    name: " galaxy J",
-    brand: "Samsung"
-  },
-  {
-    id: "479312",
-    name: " iPhone 6s",
-    brand: "Apple"
-  }
-];
+
   constructor() { }
 
   ngOnInit() {
