9912e58f53cb824c5c58ba1a99be6e12f2dbbc46
Monden8 <soma.feigl@gmail.com>
2019-09-06 01:45:33 +0200

    homepage navbar in progress css

--- Frontend/telenor-webshop/src/app/Components/homepage/homepage.component.ts
+++ Frontend/telenor-webshop/src/app/Components/homepage/homepage.component.ts
@@ -10,7 +10,8 @@ import { DialogComponent } from '../dialog/dialog.component';
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent implements OnInit {
-  items: Array<any>;
+  p: number = 1;
+  collection: Array<any>;
 
   constructor(private authsvc: AuthService, private item: ItemService, private dialog: MatDialog) { }
 
@@ -19,7 +20,7 @@ export class HomepageComponent implements OnInit {
   }
   getAllItems() {
     this.item.getItems().subscribe(res => {
-      this.items = res
+      this.collection = res
     })
   }
   addToCart(id) {
