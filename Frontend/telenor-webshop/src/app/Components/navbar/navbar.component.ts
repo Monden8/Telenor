007a48b1bb2ad0578a259d652babcd827b76fb5c
Monden8 <soma.feigl@gmail.com>
2019-09-05 13:37:43 +0200

    fixed basic nav-bar

--- Frontend/telenor-webshop/src/app/Components/navbar/navbar.component.ts
+++ Frontend/telenor-webshop/src/app/Components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/Services/auth.service';
+import { MatDialog } from '@angular/material';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authsvc: AuthService, private dialog: MatDialog, ) { }
 
   ngOnInit() {
-  }
 
+  }
+  isLoggedIn() {
+    return !this.authsvc.isLoggedIn();
+  }
+  logout() {
+    this.authsvc.logout().subscribe()
+  }
 }
--- Frontend/telenor-webshop/src/app/app.module.ts
+++ Frontend/telenor-webshop/src/app/app.module.ts
@@ -1,28 +1,82 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomepageComponent } from './Components/homepage/homepage.component';
-import { NavbarComponent } from './Components/navbar/navbar.component';
-import { RegisterComponent } from './Components/register/register.component';
 import { LoginComponent } from './Components/login/login.component';
+import { RegisterComponent } from './Components/register/register.component';
+import { HomepageComponent } from './Components/homepage/homepage.component';
+// import { SearchBarComponent } from './Components/search-bar/search-bar.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AuthService } from "./Services/auth.service";
+// import { AuthenticationInterceptor } from "./Au.interceptor";
+import { AuthGuard } from './Guards/auth.guard';
 import { DialogComponent } from './Components/dialog/dialog.component';
+import { MatCardModule } from "@angular/material/card";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatListModule } from "@angular/material/list";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatSelectModule } from "@angular/material/select";
+import {
+  MatButtonModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatRippleModule,
+  MatCheckboxModule
+} from "@angular/material";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent,
-    NavbarComponent,
-    RegisterComponent,
     LoginComponent,
-    DialogComponent
+    RegisterComponent,
+    HomepageComponent,
+    // SearchBarComponent,
+    NavbarComponent,
+    DialogComponent,
   ],
+
   imports: [
+    MatInputModule,
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRippleModule,
+    MatCheckboxModule,
+    MatCardModule,
+    MatDividerModule,
+    MatListModule,
+    MatDialogModule,
+    HttpClientModule,
+    MatSelectModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+
+  exports: [
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRippleModule,
+    MatCheckboxModule,
+    MatSelectModule
+  ],
+  providers: [
+    AuthGuard,
+    AuthService,
+    // {
+    //   provide: HTTP_INTERCEPTORS,
+    //   useClass: AuthenticationInterceptor,
+    //   multi: true
+    // },
+  ],
+  bootstrap: [AppComponent],
+  entryComponents: [DialogComponent]
 })
 export class AppModule { }
