656b4deb25fa5203ebee19949bc3e7cf1fa3b0d6
Monden8 <soma.feigl@gmail.com>
2019-09-05 15:57:37 +0200

    made the base for the cart popup

---
+++ Frontend/telenor-webshop/src/app/Components/cart-dialog/cart-dialog.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+
+@Component({
+  selector: 'app--cart-dialog',
+  templateUrl: './cart-dialog.component.html',
+  styleUrls: ['./cart-dialog.component.css']
+})
+export class CartDialogComponent implements OnInit {
+
+  respon: Array<any>
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) data,
+    public dialogRef: MatDialogRef<CartDialogComponent>
+  ) {
+    dialogRef.disableClose = true;
+    this.respon = data
+  }
+
+  ngOnInit() { }
+
+  close() {
+    this.dialogRef.close();
+  }
+}
