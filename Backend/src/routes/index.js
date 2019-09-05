edfe3f8bd22cfdb6bb9ace33150b9c6176ca956d
nora <noraselenyi@gmail.com>
2019-09-05 17:15:31 +0200

    feat(BE): get phone details done

--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -4,6 +4,7 @@ const { userLogin } = require('../controllers/login-controller');
 const { userLogout } = require('../controllers/logout-controller');
 const { getItems } = require('../controllers/getitems-controller');
 const { getCart } = require('../controllers/getcart-controller');
+const { phoneDetails } = require('../controllers/phoneDetails-controller');
 
 
 const router = express.Router();
@@ -13,6 +14,7 @@ router.post('/login', userLogin);
 router.post('/logout', userLogout);
 router.get('/main', getItems);
 router.get('/cart', getCart);
+router.get('/main/:id', phoneDetails);
 
 
 module.exports = router;
