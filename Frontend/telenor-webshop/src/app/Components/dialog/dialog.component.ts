c129da5b8a8af5776f3f9252f12896f8022788d9
Monden8 <soma.feigl@gmail.com>
2019-09-05 13:45:51 +0200

    modified login/register/dialog

--- Frontend/telenor-webshop/src/app/Components/dialog/dialog.component.ts
+++ Frontend/telenor-webshop/src/app/Components/dialog/dialog.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from "@angular/core";
+import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
 
 @Component({
-  selector: 'app-dialog',
-  templateUrl: './dialog.component.html',
-  styleUrls: ['./dialog.component.css']
+  selector: "app-dialog",
+  templateUrl: "./dialog.component.html",
+  styleUrls: ["./dialog.component.css"]
 })
 export class DialogComponent implements OnInit {
+  errormsg: string;
+  statuscode: number;
+  constructor(
+    @Inject(MAT_DIALOG_DATA) data,
+    public dialogRef: MatDialogRef<DialogComponent>
+  ) {
+    dialogRef.disableClose = true;
 
-  constructor() { }
-
-  ngOnInit() {
+    this.errormsg = data.message;
+    this.statuscode = 400;
   }
 
+  ngOnInit() {}
+
+  close() {
+    this.dialogRef.close();
+  }
 }
--- Frontend/telenor-webshop/src/app/Components/register/register.component.ts
+++ Frontend/telenor-webshop/src/app/Components/register/register.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/Services/auth.service';
+import { MatDialog } from '@angular/material';
+import { DialogComponent } from '../dialog/dialog.component';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +11,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private router: Router, private authsvc: AuthService,private dialog: MatDialog) { }
 
-  ngOnInit() {
+  hasError(controlName: string, errorName: string) {
+    return this.form.controls[controlName].hasError(errorName);
   }
-
+  submitData() {
+    let user = {
+      username: this.form.get('username').value,
+      password: this.form.get('password').value,
+      email: this.form.get('email').value,
+      money: this.form.get('money').value,
+    }
+    this.authsvc.register(user)
+    .subscribe(
+      res => {
+        if (!res) {
+          this.dialog.open(DialogComponent, {
+            data: { message: "UserName is Taken" }
+          })
+        } else {
+          this.router.navigate(['']);
+        }
+      })
+  }
+  ngOnInit() {
+    this.form = this.fb.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+      email: ['', [Validators.required]],
+      money: ['', [Validators.required]]
+    });
+  }
 }
