17f3e0fe1aa496f651e1598a00c006738f248531
Monden8 <soma.feigl@gmail.com>
2019-09-05 13:43:20 +0200

    modified login/register/dialog

--- Frontend/telenor-webshop/src/app/Components/login/login.component.ts
+++ Frontend/telenor-webshop/src/app/Components/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/Services/authentication.service';
+import { MatDialog } from '@angular/material';
+import { DialogComponent } from '../dialog/dialog.component';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +11,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private router: Router, private authsvc: AuthenticationService,private dialog: MatDialog) { }
 
-  ngOnInit() {
+  hasError(controlName: string, errorName: string) {
+    return this.form.controls[controlName].hasError(errorName);
   }
-
+  submitData() {
+    let user = {
+      username: this.form.get('username').value,
+      password: this.form.get('password').value
+    }
+    this.authsvc.login(user).subscribe(
+      res => {
+        if (!res) {
+          this.dialog.open(DialogComponent, {
+            data: { message: "Username or Password is Wrong" }
+          })
+        } else {
+          this.router.navigate(['']);
+        }
+      })
+  }
+  ngOnInit() {
+    this.form = this.fb.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    });
+  }
 }
